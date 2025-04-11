f4fad5a4b052b8b2ee8351c2428356dfc992a2f8
Chaeyoon-k <codbs9265@gmail.com>
2025-04-11 15:44:51 +0900

    Add files via upload

---
+++ 04-11 Store seller id, 리스트 수정/StoreService.java
@@ -0,0 +1,16 @@
+package com.lgy.ShoFriend.service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.lgy.ShoFriend.dto.StoreDTO;
+
+public interface StoreService {
+	
+	public ArrayList<StoreDTO> store_list(int seller_id);
+	public void store_write(HashMap<String, String> param);
+	public StoreDTO store_view(HashMap<String, String> param);
+	public void store_modify(HashMap<String, String> param);
+	public void store_delete(HashMap<String, String> param);
+	
+}
